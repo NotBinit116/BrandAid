0c3e3be724fde5bef453c425dcdf1bf719882ccc
Binit Lal Shrestha <binit.shrestha116@gmail.com>
2026-03-08 10:02:45 +0545

    Implemented JWT authentication

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from app.models.user import User
 from app.schemas.user_schema import UserCreate, UserResponse
-import hashlib
+from app.schemas.user_schema import UserLogin, TokenResponse
+from app.services.security import verify_password, create_access_token
+from sqlalchemy import select
+from app.services.security import hash_password
 
 router = APIRouter()
 
@@ -11,7 +14,7 @@ router = APIRouter()
 @router.post("/register", response_model=UserResponse)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
 
-    hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
+    hashed_password = hash_password(user.password)
 
     new_user = User(
         email=user.email,
@@ -24,3 +27,21 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@router.post("/login", response_model=TokenResponse)
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"user_id": db_user.id})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
